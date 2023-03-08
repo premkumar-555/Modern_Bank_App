1cfb5ed4009c501c9795953c7489f063b78b8403
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-03-08 17:48:42 +0530

    initialized tailwind css

--- modern-bank-app/src/App.jsx
+++ modern-bank-app/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
+import "./index.css";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Hello, world !</h1>
+    </div>
+  );
 }
 
 export default App;
